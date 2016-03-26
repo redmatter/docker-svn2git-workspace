1975b2238f56cab188c733f7c5f37b19a66dff44
Dino Korah <dino.korah@redmatter.com>
2016-03-26 10:43:56 +0000

    Configurable id_rsa with right permissions

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+: ${SSH_KEY:=/workspace/ssh-private-rsa.key}
+
 # When mounting a volume; docker ( <= v1.10.3) does not provide means to set permissions on the mount point
 # "setup-workspace" subcommand is a workaround for that
-if [ "$1" != setup-workspace ] && [ -d /workspace -a ! -w /workspace ]; then
+if [[ "$1" != setup-workspace && ( \
+    ! -w /workspace || \
+    ! -d /workspace/.ssh || \
+    ( -f "${SSH_KEY}" && ! -f /workspace/.ssh/id_rsa )
+) ]]; then
     # we need root privileges to change permission of the directory
     if ! sudo "$0" setup-workspace; then
         echo setup-workspace failed\; giving up.;
@@ -12,22 +18,33 @@ fi
 
 case "$1" in
     setup-workspace)
-        if [ ! -d /workspace/.ssh ]; then
-            mkdir /workspace/.ssh;
-            chmod go-rwx /workspace/.ssh;
-            chown migrator:migrator /workspace/.ssh;
+        if [ ! -w /workspace ]; then
+            # change permission; not recursive by purpose
+            # recursive chown can take ages if the volume has a lot of files and directories
+            exec chown migrator:migrator /workspace;
         fi
 
-        # change permission; not recursive by purpose
-        # recursive chown can take ages if the volume has a lot of files and directories
-        exec chown migrator:migrator /workspace;
+        if [ ! -d /workspace/.ssh ]; then
+            mkdir /workspace/.ssh &&
+                chmod go-rwx /workspace/.ssh &&
+                chown migrator:migrator /workspace/.ssh;
+        fi
+
+        if [[ -f "${SSH_KEY}" && ! -f /workspace/.ssh/id_rsa ]]; then
+            cp "${SSH_KEY}" /workspace/.ssh/id_rsa &&
+                chown migrator:migrator /workspace/.ssh/id_rsa &&
+                chmod go-rwx /workspace/.ssh/id_rsa;
+        fi
         ;;
+
     wait-up)
         # used when run from docker compose
-        exec "/wait-up.sh"
+        exec "/wait-up.sh";
         ;;
+
     bash|/bin/bash)
         ;;
+
     *)
         echo "Command '$1' unknown";
         exit 1;
